eb363a48fda5abc86e1206bc14e1aabbd06b6ab1
AndresSern <andrecamposer@gmail.com>
2021-02-16 16:13:18 +0000

    Update

--- console.py
+++ console.py
@@ -6,41 +6,41 @@ import cmd
 import models
 from models.base_model import BaseModel
 
+
 class HBNBCommand(cmd.Cmd):
     """ This class to setup the command interpreter """
     __classes = {"BaseModel"}
     prompt = "(hbnb) "
 
     def do_create(self, line):
-
+        """ Method To Create A New Instance"""
         arrayLine = line.split(" ")
-
         if arrayLine[0] == "":
             print("** class name missing **")
-        elif arrayLine[0] not in  self.__classes:
+        elif arrayLine[0] not in self.__classes:
             print("** class doesn't exist **")
         else:
-            my_object = eval(arrayLine[0]+ "()")
+            my_object = eval(arrayLine[0] + "()")
             print(my_object)
 
     def do_show(self, line):
+        """ Method to show all isntance"""
 
         arrayLine = line.split(" ")
         lenArray = len(arrayLine)
         if arrayLine[0] == "":
             print("** class name missing **")
-        elif arrayLine[0] not in  self.__classes:
+        elif arrayLine[0] not in self.__classes:
             print("** class doesn't exist **")
         elif (lenArray == 1):
             print("** instance id missing **")
         elif lenArray >= 2:
             searchInstance = arrayLine[0] + "." + arrayLine[1]
-            the_class =  models.storage.all()
+            the_class = models.storage.all()
             if searchInstance in the_class.keys():
                 print(the_class[searchInstance])
             else:
                 print("** no instance found **")
-
 
     def do_quit(self, line):
         "Quit command to exit the program\n"
