fe122aad94728c71f703c0585009b372d0b488c9
AndresSern <andrecamposer@gmail.com>
2021-02-16 17:52:34 +0000

    Adding the function detroy in the class

--- console.py
+++ console.py
@@ -2,14 +2,16 @@
 """
 The Line Command To The APi (BackEnd)
 """
+
 import cmd
 import models
 from models.base_model import BaseModel
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
     """ This class to setup the command interpreter """
-    __classes = {"BaseModel"}
+    __classes = {"BaseModel": BaseModel, "User" : User}
     prompt = "(hbnb) "
 
     def do_create(self, line):
@@ -17,30 +19,56 @@ class HBNBCommand(cmd.Cmd):
         arrayLine = line.split(" ")
         if arrayLine[0] == "":
             print("** class name missing **")
+            return False
         elif arrayLine[0] not in self.__classes:
             print("** class doesn't exist **")
         else:
-            my_object = eval(arrayLine[0] + "()")
-            print(my_object)
+            my_object = self.__classes[arrayLine[0]]()
+            print(my_object.id)
+            my_object.save()
 
     def do_show(self, line):
         """ Method to show all isntance"""
 
         arrayLine = line.split(" ")
         lenArray = len(arrayLine)
-        if arrayLine[0] == "":
-            print("** class name missing **")
-        elif arrayLine[0] not in self.__classes:
-            print("** class doesn't exist **")
-        elif (lenArray == 1):
-            print("** instance id missing **")
-        elif lenArray >= 2:
+
+        if (self.__check_if_exist(arrayLine, lenArray) != 1):
+
             searchInstance = arrayLine[0] + "." + arrayLine[1]
-            the_class = models.storage.all()
-            if searchInstance in the_class.keys():
-                print(the_class[searchInstance])
+            the_classes = models.storage.all()
+
+            if searchInstance in the_classes.keys():
+                print(the_classes[searchInstance])
             else:
                 print("** no instance found **")
+
+    def do_destroy(self, line):
+        """ Destroy the objects if exist"""
+        arrayLine = line.split(" ")
+        lenArray = len(arrayLine)
+
+        if (self.__check_if_exist(arrayLine, lenArray) != 1):
+
+            searchInstance = arrayLine[0] + "." + arrayLine[1]
+            the_classes = models.storage.all()
+
+            if searchInstance in the_classes.keys():
+                del the_classes[searchInstance]
+                models.storage.save()
+            else:
+                print("** no instance found **")
+
+    def __check_if_exist(self, arrayLine, lenArray):
+        if arrayLine[0] == "":
+            print("** class name missing **")
+            return 1
+        elif arrayLine[0] not in self.__classes:
+            print("** class doesn't exist **")
+            return 1
+        elif (lenArray == 1):
+            print("** instance id missing **")
+            return 1
 
     def do_quit(self, line):
         "Quit command to exit the program\n"
